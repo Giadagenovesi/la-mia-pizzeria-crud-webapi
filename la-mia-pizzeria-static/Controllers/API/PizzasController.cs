f38587d4f0bed484a417a15f5932d288e8843730
Giadagenovesi <giada.g@outlook.it>
2023-10-09 17:46:50 +0200

    Completate tutte le richieste

--- la-mia-pizzeria-static/Controllers/API/PizzasController.cs
+++ la-mia-pizzeria-static/Controllers/API/PizzasController.cs
@@ -70,7 +70,7 @@ namespace la_mia_pizzeria_static.Controllers.API
         //Creo una nuova pizza tramite chiamata API
         [HttpPost]
 
-        public IActionResult CreateNewPizza([FromBody]Pizza newPizza) 
+        public IActionResult CreateNewPizza([FromBody] Pizza newPizza)
         {
             try
             {
@@ -78,9 +78,56 @@ namespace la_mia_pizzeria_static.Controllers.API
                 _myDatabase.SaveChanges();
 
                 return Ok();
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 return BadRequest(ex);
+            }
+        }
+
+        // Modifico una pizza in base al suo id
+        [HttpPut("{id}")]
+
+        public IActionResult EditPizza(int id, [FromBody] Pizza updatedPizza)
+        {
+            Pizza? pizzaToUpdate = _myDatabase.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+            if (pizzaToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            pizzaToUpdate.Image = updatedPizza.Image;
+            pizzaToUpdate.Taste = updatedPizza.Taste;
+            pizzaToUpdate.Price = updatedPizza.Price;
+            pizzaToUpdate.Description = updatedPizza.Description;
+
+
+            _myDatabase.SaveChanges();
+
+
+            return Ok();
+
+        }
+
+        // Elimino una pizza in base al suo id
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeletePizza(int id)
+        {
+            Pizza? pizzaToDelete = _myDatabase.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+            if (pizzaToDelete != null)
+            {
+                _myDatabase.Pizzas.Remove(pizzaToDelete);
+                _myDatabase.SaveChanges();
+
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
             }
         }
     }
